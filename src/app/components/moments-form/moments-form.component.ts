ac79d30c854b09d878efc7d3f7940f1a2e3589e5
Davi <davi.frrs@gmail.com>
2023-07-25 13:40:03 -0300

    feat: validating forms

--- src/app/components/moments-form/moments-form.component.ts
+++ src/app/components/moments-form/moments-form.component.ts
@@ -1,11 +1,41 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-moments-form',
   templateUrl: './moments-form.component.html',
   styleUrls: ['./moments-form.component.scss']
 })
-export class MomentsFormComponent {
+export class MomentsFormComponent implements OnInit {
+  
   @Input() btnText!: string
+
+  momentForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl(''),
+    })
+  }
+
+  get title () {
+    return this.momentForm.get('title')!;
+  }
+
+  get description () {
+    return this.momentForm.get('description')!;
+  }
+
+  submit() {
+    if (this.momentForm.invalid) {
+      return
+    }
+
+    console.log("Funcionou");
+    
+  }
 
 }
